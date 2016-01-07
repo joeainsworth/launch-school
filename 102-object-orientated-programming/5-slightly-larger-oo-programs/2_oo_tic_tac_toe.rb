10eb431354c213067e6ab6d160f646b7436efb8c
Joe Ainsworth <joe.ainsworth@me.com>
2016-01-07 23:06:43 +0000

    OO Tic Tac Toe

--- 102-object-orientated-programming/5-slightly-larger-oo-programs/2_oo_tic_tac_toe.rb
+++ 102-object-orientated-programming/5-slightly-larger-oo-programs/2_oo_tic_tac_toe.rb
@@ -1,34 +1,28 @@
 require 'pry'
 
 class Board
-  attr_accessor :board
+  attr_accessor :square
 
   def initialize
     create_board
   end
 
   def create_board
-    board = {}
-    1.upto(9) { |n| board[n] = Square.new }
-    self.board = board
+    square = {}
+    1.upto(9) { |n| square[n] = Square.new }
+    self.square = square
   end
 
-  def display_board
-    puts '     |     |     '
-    puts "  #{board[1]}  |  #{board[2]}  |  #{board[3]}  "
-    puts '     |     |     '
-    puts '-----------------'
-    puts '     |     |     '
-    puts "  #{board[4]}  |  #{board[5]}  |  #{board[6]}  "
-    puts '     |     |     '
-    puts '-----------------'
-    puts '     |     |     '
-    puts "  #{board[7]}  |  #{board[8]}  |  #{board[9]}  "
-    puts '     |     |     '
+  def empty_squares
+    square.select { |_,v| v.mark == ' ' }.keys
   end
 
-  def available_squares
-    board.select { |s,v| v.mark == ' ' }.keys
+  def empty_squares_msg
+    empty_squares.join(', ')
+  end
+
+  def board_full?
+    empty_squares.empty?
   end
 end
 
@@ -45,10 +39,26 @@ class Square
 end
 
 class Player
+  HUMAN_MARKER = 'X'
+  COMPUTER_MARKER = 'O'
+
   attr_accessor :name
 
   def initialize
     set_name
+  end
+
+  def to_s
+    name
+  end
+
+  def set_marker!(board, square_no, player)
+    case player
+    when :human
+      board.square[square_no].mark = HUMAN_MARKER
+    when :computer
+      board.square[square_no].mark = COMPUTER_MARKER
+    end
   end
 end
 
@@ -64,15 +74,15 @@ class Human < Player
     self.name = name
   end
 
-  def move
-    puts 'Place your move:'
+  def turn(board)
+    puts "Choose a square from #{board.empty_squares_msg}:"
     answer = nil
     loop do
       answer = gets.chomp.to_i
-      break if (1..9).include?(answer)
+      break if board.empty_squares.include?(answer)
       puts 'Please enter a valid number.'
     end
-    answer
+    set_marker!(board, answer, :human)
   end
 end
 
@@ -80,9 +90,18 @@ class Computer < Player
   def set_name
     self.name = %w(Alex Cathrine Greg Rachel)
   end
+
+  def turn(board)
+    square = board.empty_squares.sample
+    set_marker!(board, square, :computer)
+  end
 end
 
 class GameEngine
+  WINNING_LINES = [1, 2, 3], [4, 5, 6], [7, 8, 9],
+                  [1, 4, 7], [2, 5, 8], [3, 6, 9],
+                  [1, 5, 9], [7, 5, 3]
+
   attr_accessor :board, :human, :computer
 
   def initialize
@@ -96,13 +115,50 @@ class GameEngine
     system 'clear'
   end
 
+  def display_board
+    puts '     |     |     '
+    puts "  #{board.square[1]}  |  #{board.square[2]}  |  #{board.square[3]}  "
+    puts '     |     |     '
+    puts '-----------------'
+    puts '     |     |     '
+    puts "  #{board.square[4]}  |  #{board.square[5]}  |  #{board.square[6]}  "
+    puts '     |     |     '
+    puts '-----------------'
+    puts '     |     |     '
+    puts "  #{board.square[7]}  |  #{board.square[8]}  |  #{board.square[9]}  "
+    puts '     |     |     '
+    puts ''
+  end
+
+  def board_full?
+    board.board_full?
+  end
+
+  def calculate_winner
+    ['X', 'O'].each do |player|
+      WINNING_LINES.each do |line|
+        return player if line.count { |no| board.square[no].mark == player } == 3
+      end
+    end
+    nil
+  end
+
+
+  def winner?
+    calculate_winner
+  end
+
   def play
     clear_screen
     loop do
-      human.move
+      display_board
+      human.turn(board)
       pry
-      break
+      break if board_full? || winner?
+      computer.turn(board)
+      break if board_full? || winner?
     end
+    display_board
   end
 
 end
