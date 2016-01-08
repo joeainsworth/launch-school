bcbd5c49ed992e15e57b8961003c4681f55a4179
Joe Ainsworth <joe.ainsworth@me.com>
2016-01-08 23:11:28 +0000

    Refactoring TTT

--- 102-object-orientated-programming/5-slightly-larger-oo-programs/2_oo_tic_tac_toe.rb
+++ 102-object-orientated-programming/5-slightly-larger-oo-programs/2_oo_tic_tac_toe.rb
@@ -1,6 +1,14 @@
 require 'pry'
 
+module Display
+  def clear_screen
+    system 'clear'
+  end
+end
+
 class Board
+  include Display
+
   attr_accessor :square
 
   def initialize
@@ -11,6 +19,22 @@ class Board
     square = {}
     1.upto(9) { |n| square[n] = Square.new }
     self.square = square
+  end
+
+  def display_board
+    clear_screen
+    puts '     |     |     '
+    puts "  #{square[1]}  |  #{square[2]}  |  #{square[3]}  "
+    puts '     |     |     '
+    puts '-----------------'
+    puts '     |     |     '
+    puts "  #{square[4]}  |  #{square[5]}  |  #{square[6]}  "
+    puts '     |     |     '
+    puts '-----------------'
+    puts '     |     |     '
+    puts "  #{square[7]}  |  #{square[8]}  |  #{square[9]}  "
+    puts '     |     |     '
+    puts
   end
 
   def empty_squares
@@ -27,6 +51,8 @@ class Board
 end
 
 class Square
+  INITIAL_MARKER = ' '
+
   attr_accessor :mark
 
   def initialize
@@ -35,6 +61,10 @@ class Square
 
   def to_s
     mark
+  end
+
+  def marked?
+    marker != INITIAL_MARKER
   end
 end
 
@@ -71,7 +101,7 @@ class Human < Player
       break unless answer.empty?
       puts 'Please enter a valid name.'
     end
-    self.name = name
+    self.name = answer
   end
 
   def turn(board)
@@ -88,7 +118,7 @@ end
 
 class Computer < Player
   def set_name
-    self.name = %w(Alex Cathrine Greg Rachel)
+    self.name = %w(Alex Cathrine Greg Rachel).sample
   end
 
   def turn(board)
@@ -98,6 +128,8 @@ class Computer < Player
 end
 
 class GameEngine
+  include Display
+
   WINNING_LINES = [1, 2, 3], [4, 5, 6], [7, 8, 9],
                   [1, 4, 7], [2, 5, 8], [3, 6, 9],
                   [1, 5, 9], [7, 5, 3]
@@ -111,54 +143,82 @@ class GameEngine
     @computer = Computer.new
   end
 
-  def clear_screen
-    system 'clear'
-  end
-
-  def display_board
-    puts '     |     |     '
-    puts "  #{board.square[1]}  |  #{board.square[2]}  |  #{board.square[3]}  "
-    puts '     |     |     '
-    puts '-----------------'
-    puts '     |     |     '
-    puts "  #{board.square[4]}  |  #{board.square[5]}  |  #{board.square[6]}  "
-    puts '     |     |     '
-    puts '-----------------'
-    puts '     |     |     '
-    puts "  #{board.square[7]}  |  #{board.square[8]}  |  #{board.square[9]}  "
-    puts '     |     |     '
-    puts ''
-  end
-
   def board_full?
     board.board_full?
   end
 
-  def calculate_winner
-    ['X', 'O'].each do |player|
+  def winning_marker
+    mark_count = nil
+    [Player::HUMAN_MARKER, Player::COMPUTER_MARKER].each do |player|
       WINNING_LINES.each do |line|
-        return player if line.count { |no| board.square[no].mark == player } == 3
+        mark_count = line.count { |no| board.square[no].mark == player }
+        return player if mark_count == 3
       end
     end
     nil
   end
 
+  def someone_won
+    case winning_marker
+    when Player::HUMAN_MARKER
+      human
+    when Player::COMPUTER_MARKER
+      computer
+    end
+  end
 
   def winner?
-    calculate_winner
+    winning_marker
+  end
+
+  def display_winner_msg
+    if winner?
+      puts "#{someone_won} won!"
+    else
+      puts 'It was a tie!'
+    end
+  end
+
+  def play_again?
+    puts 'Would you like to play again? (Y/n)'
+    answer = nil
+    loop do
+      answer = gets.chomp
+      break if %w(y n).include?(answer)
+      puts 'Please enter a valid option.'
+    end
+    answer == 'y'
+  end
+
+  def new_game
+    self.board = Board.new
+  end
+
+  def display_welcome_msg
+    clear_screen
+    puts "Welcome to the Tic Tac Toe #{human}!"
+    sleep(1)
+    puts "#{human}'s marker is: #{Player::HUMAN_MARKER}"
+    sleep(1)
+    puts "#{computer}'s marker is: #{Player::COMPUTER_MARKER}"
+    sleep(2)
   end
 
   def play
-    clear_screen
+    display_welcome_msg
     loop do
-      display_board
-      human.turn(board)
-      pry
-      break if board_full? || winner?
-      computer.turn(board)
-      break if board_full? || winner?
+      loop do
+        board.display_board
+        human.turn(board)
+        break if board_full? || winner?
+        computer.turn(board)
+        break if board_full? || winner?
+      end
+      board.display_board
+      display_winner_msg
+      break unless play_again?
+      new_game
     end
-    display_board
   end
 
 end
