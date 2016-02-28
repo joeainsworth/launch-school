abe15bfd1a29c000ee49545af44586db0a53a0c4
Joe Ainsworth <joe.ainsworth@me.com>
2016-02-28 20:54:00 +0000

    Fibonacci numbers

---
+++ exam_practice/fibonacci.rb
@@ -0,0 +1,18 @@
+def fibonacci_sequence(max_num)
+  sequence = [1, 2]
+  loop do
+    fibonacci = sequence[-2] + sequence[-1]
+    break if fibonacci >= max_num
+    sequence << fibonacci
+  end
+  sequence
+end
+
+# array size
+max_num = 100
+numbers = []
+1.upto(max_num) { |num| numbers << num }
+fibonacci = fibonacci_sequence(max_num)
+
+# select fibonacci numbers and assign array to variable
+fibonacci_numbers = numbers.select { |num| fibonacci.include?(num) }
